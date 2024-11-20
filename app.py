b22a6897c4984a50f6471e7f8b3f43f43ebecc79
Aidar199 <aidar-199@yandex.ru>
2024-11-20 11:11:47 +0500

    Начало

---
+++ app.py
@@ -0,0 +1,13 @@
+import requests
+import requests.auth
+
+
+session = requests.Session()
+
+basic = requests.auth.HTTPBasicAuth('Айдар'.encode('UTF-8'), '')
+
+#session.auth = ('Айдар', '')
+#auth = session.post('http://localhost/client')
+res = session.get('http://localhost/client/hs/client/clients?name=Кармаскал', auth=basic)
+print(res.status_code)
+print(res.text)
